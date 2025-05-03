7f325018f0f250e3abe5b20a7f22435958f162c5
shaliniparvathareddy <parvathareddyshalinireddya@gmail.com>
2025-05-03 22:33:59 +0530

    Update graphalgorithms.cpp

--- Lab 12/graphalgorithms.cpp
+++ Lab 12/graphalgorithms.cpp
@@ -1,134 +1,125 @@
-#include <iostream>
-#include <vector>
+//Program to implement graph using adjacency matrix
+#include <cstdio>
+#include<iostream>
 using namespace std;
 class graph{
     private:
-    int v;
-    vector<vector<pair<int,int>>> adj;
-    vector<tuple<int,int,int>> edge;
+    int numvertices;
+    int matrix[20][20];
     public:
-    graph(int vertices):v(vertices){
-        adj.resize(v);
+    graph(int vertices){
+        if(numvertices>20){
+            cout<<"Max allowed vertices 20"<<"\n";
+            vertices = 20;
+        }
+        numvertices = vertices;
+        for(int i=0;i<numvertices;i++){
+            for(int j=0;j<numvertices;j++){
+                matrix[i][j]=0;
+            }
+        }
     }
-    int addedge(int,int,int);
-    int prims();
-    int findparent(vector<int>& parent,int u);
-    int kruskals();
-    int dijksta(int src);
+    int insert(int,int);
+    int delete1(int,int);
+    int search(int,int);
+    void display();
 };
 int main(){
+    int choice;
+    int src;
+    int dest;
     int vertices;
     cout<<"Enter number of vertices:";
     cin>>vertices;
     graph g1(vertices);
     while(1){
-        cout << "\n--- Graph Menu ---\n";
-        cout << "1. Add Edge\n";
-        cout << "2. Prim's MST\n";
-        cout << "3. Kruskal's MST\n";
-        cout << "4. Dijkstra's Shortest Path\n";
-        cout << "5. Exit\n";
-        cout << "Enter choice: ";
-        cin >> choice;
+        printf("Enter 1 for insert\nEnter 2 for delete\nEnter 4 for search\n");
+        printf("Enter 4 for display\nEnter 5 for exit\nEnter your choice:");
+        scanf("%d",&choice);
         switch(choice){
             case 1:
-            int u, v, w;
-                cout << "Enter edge (u v weight): ";
-                cin >> u >> v >> w;
-                g1.addedge(u,v,w);
-                break;
+            printf("Enter the src vertex to insert:");
+            scanf("%d",&src);
+            printf("Enter the dest vertex: to insert");
+            scanf("%d",&dest);
+            g1.insert(src,dest);
+            break;
             case 2:
-            g1.prims();
+            printf("Enter the src vertex to delete:");
+            scanf("%d",&src);
+            printf("Enter the dest vertex to delete:");
+            scanf("%d",&dest);
+            g1.delete1(src,dest);
             break;
             case 3:
-            g1.kruskals();
+            printf("Enter the src vertex to search for:");
+            scanf("%d",&src);
+            printf("Enter the dest vertex to search for:");
+            scanf("%d",&dest);
+            g1.search(src,dest);
             break;
             case 4:
-            int src;
-            cout << "Enter source vertex: ";
-            cin >> src;
-            g1.dijksta(src);
+            g1.display();
             break;
             case 5:
             return 0;
             default:
-            cout<<"Invalid choice\n";
+            printf("Enter valid choice\n");
         }
+        printf("\n");
+        printf("\n");
     }
     return 0;
 }
-int graph:: addedge(int u,int V,int w){
-    if(u>=v || V>=v || u<0 || V<0){
-        cout<<"Invalid vertex\n";
-        return 0;
+//Function to insert elements
+int graph::insert(int src,int dest){
+    if(src > numvertices || src < 0 || dest > numvertices || dest < 0){
+        cout<<"Enter valid src and dest values";
+        return -1;
     }
-    adj[u].push_back({V,w});
-    adj[V].push_back({u,w});
-    edge.push_back({u,V,w});
+    matrix[src][dest] = 1;
+    matrix[dest][src] = 1;
+    cout<<"inserted successfully!!!";
     return 1;
 }
-int graph::prims(){
-    vector <bool> visited(v.false);
-    vector <int>parent(v,-1);
-    vector<bool> key(v,INF);
-    key[0] = 0;
-    pq.push({0,0});
-    while(!pq.empty(())){
-        int u = pq.top().second;
-        pq.pop();
-        if(visted[u]) {
-            continue;
-        }
-        visited[u] = true;
-        for(auto [h,weight] : adj[u]){
-            if(!visited[h] && weight < key[h]){
-                key[h] = weight;
-                parent[h] = u;
-                pq.push({key[h],h});
-            }
-        }
+//Function to delete elements
+int graph::delete1(int src,int dest){
+    if(src > numvertices || src < 0 || dest > numvertices || dest < 0){
+        cout<<src<<"and/or"<<dest<<"vertices dont exist"<<"\n";
+        return -1;
     }
+    if(matrix[src][dest]!=1){
+        cout<<src<<"and"<<dest<<"are not connected"<<"\n";
+        return -1;
+    }
+    matrix[src][dest] = 0;
+    matrix[dest][src] = 0;
+    return 1;
 }
-int graph::findparent(vector<int>& parent,int u){
-    if(parent[u] != u){
-        parent[u] = findparent(parent,parent[u]);
+//Function to search for elements
+int graph::search(int src,int dest){
+    if(src > numvertices || src < 0 || dest > numvertices || dest < 0){
+        cout<<src<<"and/or"<<dest<<"vertices dont exist"<<"\n";
+        return -1;
     }
-    return parent[u];
+    if(matrix[src][dest] == 1){
+        cout<<"Edge exists between"<<src<<"and"<<dest<<"\n";
+        return 1;
+    }
+    cout<<"Edge does not exist between"<<src<<"and"<<dest<<"\n";
+    return -1;
 }
-int graph::kruskals(){
-    sort(edges.begin(),edges.end());
-    vector<int> parent(v);
-    for(int i=0;i<v;i++){
-        parent[i] = i;
+//Function to display elements
+void graph::display(){
+    for(int i=0;i<numvertices;i++){
+        cout<<i<<"   ";
     }
-    vector <pair<int,int>> mst;
-    for(auto [w,u,v] :edges){
-        int pu = findparent(parent,u);
-        int pv = findparent(parent,v);
-        if(pu != pv){
-            mst.push_back({u,v});
-            parent[pu] = pv;
+    cout<<"\n";
+    for(int i=0;i<numvertices;i++){
+        cout<<i<<"   ";
+        for(int j=0;j<numvertices;j++){
+            cout<<matrix[i][j]<<"   ";
         }
-    }
-}
-int graph::dijksta(int src){
-    if(src >= v || src < 0){
-        cout<<"Invalid source vertex\n";
-        return;
-    }
-    vector <int> dist(v,INF);
-    dist[src] = 0;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-    pq.push({0,src});
-    while(!pq.empty()){
-        int u = pq.top().second;
-        int d = pq.top().first;
-        pq.pop();
-        for(auto[v,w];adj[u]){
-            if(dist[v] > d+w){
-                dist[v] = d+w;
-                pq.push({dist[v],v});
-            }
-        }
+        cout<<"\n";
     }
 }
